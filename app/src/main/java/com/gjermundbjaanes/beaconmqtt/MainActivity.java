a39912a810abca93d0e62e045c6eb807790183e8
Gjermund Bjaanes <bjaanes@gmail.com>
2017-01-09 17:45:39 +0100

    Added first version of settings

--- app/src/main/java/com/gjermundbjaanes/beaconmqtt/MainActivity.java
+++ app/src/main/java/com/gjermundbjaanes/beaconmqtt/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gjermundbjaanes.beaconmqtt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -44,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            this.startActivity(settingsIntent);
             return true;
         }
 
